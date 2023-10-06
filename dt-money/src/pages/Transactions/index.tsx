be74c38682fcf6a7e15b2d90976e903c3b0e6f0b
Gabriel Sales <gabrielsales081@gmail.com>
2023-10-06 00:09:37 -0300

    feat: add header, table and summary

---
+++ dt-money/src/pages/Transactions/index.tsx
@@ -0,0 +1,38 @@
+import { Header, Summary } from "../../components";
+import * as S from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <S.TransactionsContainer>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighLight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Méqui</td>
+              <td>
+                <S.PriceHighLight variant="outcome">
+                  - R$ 59,00
+                </S.PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>09/04/2023</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
+    </div>
+  );
+}
