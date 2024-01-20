fb4e0bd7bede2e7189420088f0f6dee7301974cf
Gabriel Sales <gabrielsales081@gmail.com>
2024-01-20 15:33:38 -0300

    feat: seach transactions

--- dt-money/src/contexts/TransactionsContext.tsx
+++ dt-money/src/contexts/TransactionsContext.tsx
@@ -11,31 +11,40 @@ interface ITransaction {
 
 interface ITransactionContextType {
   transactions: ITransaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface ITransactionProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<ITransactionContextType>({
-  transactions: [],
-});
+export const TransactionsContext = createContext({} as ITransactionContextType);
 
 export function TransactionsProvider({ children }: ITransactionProviderProps) {
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
-  async function loadTransactions() {
-    const res = await fetch("http://localhost:3333/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3333/transactions");
+    if (query) {
+      url.searchParams.append("q", query);
+    }
+
+    const res = await fetch(url);
     const data = await res.json();
 
     setTransactions(data);
   }
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+      }}
+    >
       {children}
     </TransactionsContext.Provider>
   );
