abb7e18216d4119522390bb5455dcea104fa6328
Gabriel Sales <gabrielsales081@gmail.com>
2024-01-20 15:45:05 -0300

    feat: add and setting axios

--- dt-money/src/contexts/TransactionsContext.tsx
+++ dt-money/src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface ITransaction {
   id: number;
@@ -24,15 +25,13 @@ export function TransactionsProvider({ children }: ITransactionProviderProps) {
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL("http://localhost:3333/transactions");
-    if (query) {
-      url.searchParams.append("q", query);
-    }
+    const res = await api.get("/transactions", {
+      params: {
+        q: query,
+      },
+    });
 
-    const res = await fetch(url);
-    const data = await res.json();
-
-    setTransactions(data);
+    setTransactions(res.data);
   }
   useEffect(() => {
     fetchTransactions();
